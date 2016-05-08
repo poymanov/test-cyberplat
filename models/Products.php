1f8bf7611396007ce09ad9a2c422c5acdd0b4f3d
Nikolay <n.poymanov@gmail.com>
2016-05-08 16:26:24 +0300

    [new] display product catagory
    Отображение категории товара на странице управления товарами

--- models/Products.php
+++ models/Products.php
@@ -12,4 +12,28 @@ class Products extends ActiveRecord
             [['name'], 'required']            
         ];
     }
+
+    public function getCategoryName() {
+    	// Получаем имя родительской категории по id товара
+
+    	$category_id = $this->category_id;
+    	// Если не указан id родительской категории возвращаем пустое значение
+    	if($category_id) {
+
+    		$parent = Catalog::findOne($category_id);
+
+    		if ($parent === null) {
+    			// Если не нашли родительской категории с таким id, 
+    			// возвращаем пустое значение
+            	return "";
+        	} else {
+        		// Иначе возвращаем имя родительской категории
+        		return $parent->name;
+        	}
+
+    	} else {
+    		return "";
+    	}
+    	
+    }
 }
--- views/products/index.php
+++ views/products/index.php
@@ -24,7 +24,7 @@ $this->title = 'Управление товарами';
 			<tr>
 				<td><?php echo $product->id; ?></td>
 				<td><?php echo $product->name; ?></td>
-				<td><?php //echo $product->getCategoryName(); ?></td>
+				<td><?php echo $product->getCategoryName(); ?></td>
 				<td>
 					<a href="/products/update/<?php echo $product->id; ?>" class="glyphicon glyphicon-pencil"></a>
 				</td>
